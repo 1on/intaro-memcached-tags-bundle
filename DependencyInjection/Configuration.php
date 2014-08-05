acbe43bbeef80a4e7dca9960819f42ddbeaedaab
Kruglov Kirill <kruglov@intaro.ru>
2014-08-05 12:30:18 +0400

    fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Intaro\NativeQueryBuilderBundle\DependencyInjection;
+namespace Intaro\MemcachedTagsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
